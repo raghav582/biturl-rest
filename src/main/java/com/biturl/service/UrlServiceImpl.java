de2e00d4ece0a5f766739ae6ba4acdf462e32e0d
raghav582 <raghavsharma582@gmail.com>
2020-12-23 22:45:30 +0530

    persist

---
+++ src/main/java/com/biturl/service/UrlServiceImpl.java
@@ -0,0 +1,12 @@
+package com.biturl.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UrlServiceImpl implements UrlService{
+
+	public String addUrl(final String url) {
+		String urlCode = "";
+		return urlCode;
+	}
+}
