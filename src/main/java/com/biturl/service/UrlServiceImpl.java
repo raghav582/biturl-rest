cda0887cf1cd7bf1b60fea77d23dc0cf1abf2a29
raghav582 <raghavsharma582@gmail.com>
2020-12-25 16:08:02 +0530

    service

--- src/main/java/com/biturl/service/UrlServiceImpl.java
+++ src/main/java/com/biturl/service/UrlServiceImpl.java
@@ -1,12 +1,33 @@
 package com.biturl.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.biturl.adaptor.UrlAdaptor;
+import com.biturl.model.UrlModel;
+import com.biturl.persistence.entity.UrlEntity;
+import com.biturl.persistence.service.PersistenceService;
+import com.biturl.urlcode.UrlCode;
 
 @Service
 public class UrlServiceImpl implements UrlService{
 
-	public String addUrl(final String url) {
-		String urlCode = "";
-		return urlCode;
+	@Autowired private PersistenceService persistenceService;
+	
+	@Override
+	public String addUrl(final UrlModel model) {
+		final UrlEntity entity = UrlAdaptor.toEntity(model);
+		Long id = persistenceService.save(entity).getId();
+		return idToCode(id);
+	}
+	
+	public String idToCode(final Long id) {
+		String code = "";
+		Integer entityId = id.intValue();
+		for(int i=0; i<6; i++) {
+			code = UrlCode.URL_CODE[entityId%62] + code;
+		}
+		
+		return code;
 	}
 }
