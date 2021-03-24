402b40df926450550c4574b3195078f35b1a3561
raghav582 <raghavsharma582@gmail.com>
2021-03-24 21:59:31 +0530

    cross origin

--- src/main/java/com/biturl/controller/UrlController.java
+++ src/main/java/com/biturl/controller/UrlController.java
@@ -3,6 +3,7 @@ package com.biturl.controller;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.condition.ConsumesRequestCondition;
 
+import com.biturl.model.ShortUrlModel;
 import com.biturl.model.UrlModel;
 import com.biturl.service.UrlService;
 
@@ -21,14 +23,19 @@ public class UrlController {
 	private static final Logger LOGGER = LogManager.getLogger(UrlController.class);
 	
 	@RequestMapping(value = "/add-url",
-			method = RequestMethod.POST)
+			method = RequestMethod.POST,
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
-	public String addUrl(@RequestBody final UrlModel model) {
+	public ShortUrlModel addUrl(@RequestBody final UrlModel model) {
+		final ShortUrlModel shortUrlModel = new ShortUrlModel();
 		try{
-			return urlService.addUrl(model);
+			shortUrlModel.setUrl(urlService.addUrl(model));
 		} catch (Exception e) {
 			LOGGER.error(e.getMessage());
-			return "";
+			shortUrlModel.setUrl("");
 		}
+		
+		return shortUrlModel;
 	}
 }
---
+++ src/main/java/com/biturl/model/ShortUrlModel.java
@@ -0,0 +1,15 @@
+package com.biturl.model;
+
+public class ShortUrlModel {
+
+	private String url;
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(final String url) {
+		this.url = url;
+	}
+	
+}
